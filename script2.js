5324be9af9a94512412f38694acba823cc33522c
Jonathan Pharies <pharies@Jonathans-MacBook-Pro.local>
2019-10-30 21:36:14 -0400

    done

--- script2.js
+++ script2.js
@@ -48,7 +48,7 @@ var fail = function(data)
 var createSideBar = function(pokemon)
 {
     
-        removeSide()
+       removeSide()
        d3.select("#pokemonholder")
         .selectAll("div")
         .data(pokemon)
@@ -201,7 +201,9 @@ var removeSide = function()
 // search bar functions
 var makeAlphebet = function(data)
 {
-        console.log(data)
+        
+    
+    
  var search = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "all"]
  
  d3.select("#searchBar").selectAll("span").data(search).enter().append("span").text(function(d){return d}).attr("class", "searchKey")
